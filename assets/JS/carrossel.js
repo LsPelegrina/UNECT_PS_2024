0e4b4d9418a887cedcd8aacea1080db75686a8c2
Guilherme Renato Terra De Macedo <guirenato1181@gmail.com>
2024-03-12 00:19:05 -0300

    alt no corpo html e carrossel

--- assets/JS/carrossel.js
+++ assets/JS/carrossel.js
@@ -1,11 +1,11 @@
 
 $(function(){
 
-    $("#sec-4").slick({
+    $(".shelf-list").slick({
         dots: false,
         infinite: false,
         speed: 300,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         arrows: false,
         responsive: [
@@ -32,9 +32,6 @@ $(function(){
               slidesToScroll: 1
             }
           }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
         ]
     });
 })
